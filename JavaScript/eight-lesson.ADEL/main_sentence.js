2a6b3cbd02b7ca8022ce3e5ca1c3c1d968ab8ab7
Yanainu <yanaworkin@gmail.com>
2021-10-11 18:43:56 +0300

    added resolve for sentences

--- JavaScript/eight-lesson.ADEL/main_sentence.js
+++ JavaScript/eight-lesson.ADEL/main_sentence.js
@@ -44,40 +44,67 @@
 // isPalindromSentence()
 
 
+//const str = prompt('введите предложение').toLowerCase(); //делаем все с маленькой буквы;
 
-
-const str = prompt('введите предложение').toLowerCase(); //делаем все с маленькой буквы;
-
-function isPalindromSentence() {
+// function isPalindromSentence() {
     
-    let result = 'Палиндром'; //по умолчанию
+//     let result = 'Палиндром'; //по умолчанию
 
-    let indexForStart = 0;
-    let indexForEnd = str.length - 1;
+//     let indexForStart = 0;
+//     let indexForEnd = str.length - 1;
 
-    // ЦИКЛ
+//     // ЦИКЛ
+//     while (str) {
+//         if (((/[a-zA-Z]/).test(str[indexForStart])) !== true) {//если слева не буква
+//             indexForStart += 1;
+//         } else if (((/[a-zA-Z]/).test(str[indexForEnd])) !== true) {//если справа не буква
+//             indexForEnd -= 1;
+//         } else {//если уже обе буквы
+//             if ((str[indexForStart] === str[indexForEnd])) {
+//                 indexForStart += 1;
+//                 indexForEnd -= 1;
+//             } else {
+//                 result = 'НЕ палиндром';
+//                 break;
+//             }
+//         }
+        
+//     } 
 
+//     alert(result);
+// }
 
-    while (str) {
-        if (((/[a-zA-Z]/).test(str[indexForStart])) !== true) {//если слева не буква
-            indexForStart += 1;
-        } else if (((/[a-zA-Z]/).test(str[indexForEnd])) !== true) {
-            indexForEnd -= 1;
+//решение 
+
+const str = prompt('введите предложение').toLowerCase();
+ 
+function isPalindromSentence(str) {
+    
+    let result = 'Палиндром';
+ 
+    let left = 0;
+    let right = str.length - 1;
+ 
+    while (right >= left) {
+        if (!(/[a-zA-Zа-яА-Я]/).test(str[left])) {
+            left += 1;
+            continue;
+        }
+        if (!(/[a-zA-Z]/).test(str[right])) {
+            right -= 1;
+            continue;
+        } 
+
+        if ((str[left] === str[right])) {
+            left += 1;
+            right -= 1;
         } else {
-            if ((str[indexForStart] === str[indexForEnd])) {
-                indexForStart += 1;
-                indexForEnd -= 1;
-            } else {
-                result = 'НЕ палиндром';
-                break;
-            }
+            result = 'НЕ палиндром';
+            break;
         }
 
-        //сравнение буков
+    }
 
-        
-    } 
-
-    alert(result);
+    alert(result)
 }
-isPalindromSentence()
+isPalindromSentence(str)
