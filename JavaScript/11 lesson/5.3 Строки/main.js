69db4d11036a899c33d65b4a71b1e641b29a272a
Yanainu <yanaworkin@gmail.com>
2021-11-27 18:44:59 +0300

    corrected

--- JavaScript/11 lesson/5.3 Строки/main.js
+++ JavaScript/11 lesson/5.3 Строки/main.js
@@ -1,16 +1,6 @@
 'use strict'
 
-//////////////////////////////ЗАДАЧА 1
-// Напишите функцию ucFirst(str), возвращающую строку str с заглавным первым символом. Например:
-
-// ucFirst("вася") == "Вася";
-
-function ucFirst(str) {
-    let result = str[0].toLocaleUpperCase() + str.slice(1);
-    return result;
-}
-
-//////////////////////////////ЗАДАЧА 2
+//-----------------------------------------------------------------------------------------ЗАДАЧА 2
 // Напишите функцию checkSpam(str), возвращающую true, 
 //если str содержит 'viagra' или 'XXX', а иначе false.
 
@@ -20,10 +10,16 @@ checkSpam('buy ViAgRA now') == true
 checkSpam('free xxxxx') == true
 checkSpam("innocent rabbit") == false
 
+//ИСПРАВЛЕНО
+//что такое check1 и check2? скинь потом корректное решение
+
 function checkSpam(str) {
     str = str.toLocaleLowerCase();
 
-    if (str.includes(check1.toLocaleLowerCase()) || str.includes(check2.toLocaleLowerCase())) {
+    let check1 = 'viagra';
+    let check2 = 'xxx';
+
+    if (str.includes(check1) || str.includes(check2)) {
         return true;
     } else {
         return false;
@@ -31,8 +27,8 @@ function checkSpam(str) {
 }
 
 
+//-----------------------------------------------------------------------------------------ЗАДАЧА 3
 
-//////////////////////////////ЗАДАЧА 3
 // Создайте функцию truncate(str, maxlength), которая проверяет длину строки str и, 
 // если она превосходит maxlength, заменяет конец str на "…", так, 
 // чтобы её длина стала равна maxlength.
@@ -46,28 +42,15 @@ truncate("Вот, что мне хотелось бы сказать на эту
 
 truncate("Всем привет!", 20) = "Всем привет!"
 
+// //ИСПРАВЛЕНО:
+// должен быть return, не console.log
+// else тут не нужен, если внутри if будет return
+
 function truncate(sentence, maxlengh) {
     if (sentence.length > maxlength) {
         const sentenceCropped = sentence.slice(0, maxlength - 1) + '...';
-        console.log(sentenceCropped);
-    } else {
-        console.log(sentence);
-    }
+        return sentenceCropped;
+    } 
+
+    return sentence;
 }
-
-
-
-//////////////////////////////ЗАДАЧА 4
-// Есть стоимость в виде строки "$120". То есть сначала идёт знак валюты, а затем – число.
-
-// Создайте функцию extractCurrencyValue(str), которая будет из такой строки выделять 
-// числовое значение и возвращать его.
-
-// Например:
-
-alert( extractCurrencyValue('$120') === 120 ); // true
-
-function extractCurrencyValue(string) {
-    return string = parseFloat(string.slice(1));//так отсечется начальный знак валюты + если потом будут символы их парс уберет
-}
-//ну в учебнике просто отсекли первый. ладно
--- JavaScript/11 lesson/5.5 Методы массивов/main.js
+++ JavaScript/11 lesson/5.5 Методы массивов/main.js
@@ -24,6 +24,24 @@ function camelize(string) {
     console.log(stringUpdated);
 }
 
+//ИСПРАВЛЕНО (другое решение)
+// задача в принципе решена (за исключением того что снова должен быть return а не console.log), 
+// но как можно было бы ее решить используя методы массивов? подсказка - используй split('-'), и метод map
+
+function camelize(string) {
+
+    const splittedArray = string.split('-');
+
+    const resultArray = splittedArray.map(item => {
+        return item[0].toUpperCase() + item.slice(1);
+    })
+
+    const resultString = resultArray.join('');
+    return resultString;
+}
+
+
+
 //------------------------------------------------------------------------------- ЗАДАЧА 2
 
 // Напишите функцию filterRange(arr, a, b), которая принимает массив arr, 
@@ -52,200 +70,14 @@ let filtered1 = filterRange(arr, 1, 4);
 alert(filtered1); // 3,1 (совпадающие значения)
 alert( arr ); // 5,3,8,1 (без изменений)
 
-//------------------------------------------------------------------------------- ЗАДАЧА 3
+//ИСПРАВЛЕНО 
+// все ок, но как бы ты решила задачу с помощью метода filter?
+//РЕШЕНИЕ С ФИЛЬТРОМ
 
-// Напишите функцию filterRangeInPlace(arr, a, b), которая принимает массив arr и 
-// удаляет из него все значения кроме тех, которые находятся между a и b. 
-// То есть, проверка имеет вид a ≤ arr[i] ≤ b.
-
-// Функция должна изменять принимаемый массив и ничего не возвращать.
-
-// Например:
-
-let arr = [5, 3, 8, 1];
-
-filterRangeInPlace(arr, 1, 4); // удалены числа вне диапазона 1..4
-
-alert( arr ); // [3, 1]
-
-//аргументы иначе обозвала чтобы от задачи2 отличалось
-//вариант просто чтобы работало:
-
-function filterRangeInPlace(array, x, y) {
-    for (let i = 0; i < array.length; i += 1) {
-        if (array[i] < x || array[i] > y) {
-            array.splice(i,1);
-            i--;
-        }
-    }
-    return array;
+function filterRange (arr, a, b) {
+    filtered = arr.filter(item => item >= a && item <= b)
+    return filtered;
 }
-
-
-//------------------------------------------------------------------------------- ЗАДАЧА 4
-
-//сортировать массив по убыванию
-let arr = [5, 2, 1, -10, 8];
-
-// ... ваш код для сортировки по убыванию
-
-arr.sort( (a, b) => b - a );
-
-alert( arr ); // 8, 5, 2, 1, -10
-
-
-//------------------------------------------------------------------------------- ЗАДАЧА 5
-
-// У нас есть массив строк arr. Нужно получить отсортированную копию, но оставить arr неизменённым.
-
-// Создайте функцию copySorted(arr), которая будет возвращать такую копию.
-
-let arr = ["HTML", "JavaScript", "CSS"];
-
-let sorted = copySorted(arr);
-
-alert( sorted ); // CSS, HTML, JavaScript
-alert( arr ); // HTML, JavaScript, CSS (без изменений)
-
-//решение
-function copySorted(arr) {
-    let arrCopy = arr.concat()
-    return arrCopy.sort()
-}
-
-//------------------------------------------------------------------------------- ЗАДАЧА 6
-
-// Создайте функцию конструктор Calculator, которая создаёт «расширяемые» объекты калькулятора.
-
-// Задание состоит из двух частей.
-
-// Во-первых, реализуйте метод calculate(str), который принимает строку типа "1 + 2"
-//  в формате «ЧИСЛО оператор ЧИСЛО» (разделено пробелами) и возвращает результат. Метод должен понимать плюс + и минус -.
-
-// Пример использования:
-
-let calc = new Calculator;
-
-alert( calc.calculate("3 + 7") ); // 10
-
-function Calculator() {
-
-    this.library = {
-        '+': (...arg) => {
-            return this.sum(...arg)
-        },
-        '-': (...arg) => {
-            return this.substraction(...arg)
-        }
-    }
-
-    this.sum = function (a, b) {
-        return Number(a) + Number(b);
-    }
-
-    this.substraction = function (a, b) {
-        return Number(a) - Number(b);
-    }
-
-    this.calculate = function (string) {//принимаем строку str
-        //делаем из нее массив
-        const array = str.split(' ');//['3', '+', '7']
-       
-        const a = array[0];
-        const b = array[2];
-
-        const method = array[1];
-
-        return this.library[method](a, b);
-    
-    }
-
-    // Затем добавьте метод addMethod(name, func), который добавляет в калькулятор новые операции. Он принимает оператор name и функцию с двумя аргументами func(a,b), которая описывает его.
-    this.addMethod = function (method, func) {
-        this[method] = func;
-    }
-}
-
-// Например, давайте добавим умножение *, деление / и возведение в степень **:
-
-let powerCalc = new Calculator;
-powerCalc.addMethod("*", (a, b) => a * b);
-powerCalc.addMethod("/", (a, b) => a / b);
-powerCalc.addMethod("**", (a, b) => a ** b);
-
-let result = powerCalc.calculate("2 ** 3");
-alert( result ); // 8
-// Для этой задачи не нужны скобки или сложные выражения.
-// Числа и оператор разделены ровно одним пробелом.
-// Не лишним будет добавить обработку ошибок.
-
-//------------------------------------------------------------------------------- ЗАДАЧА 7 
-
-// У вас есть массив объектов user, и в каждом из них есть user.name. 
-// Напишите код, который преобразует их в массив имён.
-
-// Например:
-
-let vasya = { name: "Вася", age: 25 };
-let petya = { name: "Петя", age: 30 };
-let masha = { name: "Маша", age: 28 };
-
-let users = [ vasya, petya, masha ];
-
-let names = users.map(item => item.name)/* ... ваш код */
-
-alert( names ); // Вася, Петя, Маша
-
-//-------------------------------------------------------------------------------ЗАДАЧА 8
-
-// У вас есть массив объектов user, и у каждого из объектов есть name, surname и id.
-
-// Напишите код, который создаст ещё один массив объектов с параметрами id и fullName, 
-// где fullName – состоит из name и surname.
-
-// Например:
-
-let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
-let petya = { name: "Петя", surname: "Иванов", id: 2 };
-let masha = { name: "Маша", surname: "Петрова", id: 3 };
-
-let users = [ vasya, petya, masha ];
-
-let usersMapped = users.map(item => {
-    return {
-        fullname: `${item.name} ${item.surname}`,
-        id: item.id,
-    }
-})
-
-console.log(usersMapped)
-
-// usersMapped = [
-//   { fullName: "Вася Пупкин", id: 1 },
-//   { fullName: "Петя Иванов", id: 2 },
-//   { fullName: "Маша Петрова", id: 3 }
-// ]
-
-
-//------------------------------------------------------------------------------- ЗАДАЧА 9
-
-//Напишите функцию sortByAge(users), которая принимает массив объектов со свойством age и сортирует их по нему.
-
-// Например:
-
-let vasya = { name: "Вася", age: 25 };
-let petya = { name: "Петя", age: 30 };
-let masha = { name: "Маша", age: 28 };
-
-let arr = [ vasya, petya, masha ];
-
-function sortByAge(arr) {
-    arr.sort((a, b) => a.age - b.age);
-}
-
-sortByAge(arr);
-
-// теперь: [vasya, masha, petya]
 
 //------------------------------------------------------------------------------- ЗАДАЧА 10
 
@@ -296,32 +128,8 @@ function random(min, max) {
 
 //нихера не работает
 
-//------------------------------------------------------------------------------- ЗАДАЧА 11
+//ИСПРАВЛЕНО уже не вспомню что было не так, РАБОТАЕТ ))0 
 
-// Напишите функцию getAverageAge(users), которая принимает массив объектов 
-// со свойством age и возвращает средний возраст.
-
-// Формула вычисления среднего арифметического значения: (age1 + age2 + ... + ageN) / N.
-
-// Например:
-
-let vasya = { name: "Вася", age: 25 };
-let petya = { name: "Петя", age: 30 };
-let masha = { name: "Маша", age: 29 };
-
-let arr = [ vasya, petya, masha ];
-
-alert( getAverageAge(arr) ); // (25 + 30 + 29) / 3 = 28
-
-//решение
-function getAverageAge(arr) {
-    let ageSum = arr.reduce((acc, item) => {
-        acc += item.age;
-        return acc;
-    }, 0)
-
-    return averageAge = ageSum / arr.length;
-}
 
 //------------------------------------------------------------------------------- ЗАДАЧА 12
 
@@ -329,14 +137,15 @@ function getAverageAge(arr) {
 
 // Напишите функцию unique(arr), которая возвращает массив, содержащий только уникальные элементы arr.
 
-// Например:
+//ИСПРАВЛЕНО
+//можно использовать вариант проверки if(!uniqueArr.includes(item))
 
 function unique(arr) {
 
     let uniqueArr = [];
     
     arr.forEach((item) => {
-        if (uniqueArr.includes(item) == false) {
+        if (!(uniqueArr.includes(item))) {
             uniqueArr.push(item);
         }
     })
@@ -349,3 +158,6 @@ let strings = ["кришна", "кришна", "харе", "харе",
 ];
 
 alert( unique(strings) ); // кришна, харе, :-O
+
+
+
