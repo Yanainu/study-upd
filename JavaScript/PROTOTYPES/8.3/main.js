4ce6ea6a8d8e7fca5cdef52f73664c6428fb0883
Yanainu <yanaworkin@gmail.com>
2021-12-14 15:07:22 +0300

    prototypes done

--- JavaScript/PROTOTYPES/8.3/main.js
+++ JavaScript/PROTOTYPES/8.3/main.js
@@ -25,20 +25,43 @@ f.defer(1000); // выведет "Hello!" через 1 секунду
 // Добавьте всем функциям в прототип метод defer(ms), который возвращает обёртку, откладывающую вызов функции на ms миллисекунд.
 // Например, должно работать так:
 
+
+f.defer(1000)(1, 2); // выведет 3 через 1 секунду.
+// Пожалуйста, заметьте, что аргументы должны корректно передаваться оригинальной функции.
+
 function f(a, b) {
   alert( a + b );
 }
 
-  Function.prototype.defer = function(ms) {//ОЧЕНЬ ПУТАНО
-    
-    return function(a, b) {
-      setTimeout(() => {
-        return this();
-      }, ms)
-    }
+Function.prototype.defer = function(ms) {
+  return (a, b) => {
+    setTimeout(() => {
+      return this(a, b);
+    }, ms)
+  }
 }
 
+f.defer(1000)(1, 2);
 
-f.defer(1000)(1, 2); // выведет 3 через 1 секунду.
-// Пожалуйста, заметьте, что аргументы должны корректно передаваться оригинальной функции.
+//-------------------------------------------примеры для закрепления
+
+function calculate() {
+  return function(a, b) {
+    return a + b;
+  }
+}
+
+calculate()(1, 3);
+
+//------------------------------------------
+
+function calculate(a) {
+  return function(b) {
+    return function(c) {
+      return a+b+c;
+    }
+  }
+}
+
+calculate(1)(2)(4)
 
